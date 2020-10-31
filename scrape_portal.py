b1260d0ee1f6daad1dde569050bd796ebfcb6ed2
vreezy117 <vreezy117@googlemail.com>
2020-10-31 17:28:28 +0100

    clear

--- scrape_portal.py
+++ scrape_portal.py
@@ -110,38 +110,38 @@ def print_configs(config):
     print("")
     print("~"*15)
 
-def get_all_portals(login, tiles):
-    timed_out_items = []
-    portals = []
-    portal_id = []
-    tiles_data = []
-    for idx, tile in enumerate(tiles):
-        iitc_xtile = int( tile[0] )
-        iitc_ytile = int( tile[1] )
+# def get_all_portals(login, tiles):
+#     timed_out_items = []
+#     portals = []
+#     portal_id = []
+#     tiles_data = []
+#     for idx, tile in enumerate(tiles):
+#         iitc_xtile = int( tile[0] )
+#         iitc_ytile = int( tile[1] )
         
-        iitc_tile_name  = ('{0}_{1}_{2}_0_8_100').format(zoom, iitc_xtile, iitc_ytile)
-        current_tile = idx+1
-        print(str("{0}/{1} Getting portals from tile : {2}").format(current_tile, total_tiles, iitc_tile_name))
-        try:
-            tiles_data.append(login.get_entities([iitc_tile_name]))
-        except:
-            print(str("Something went wrong while getting portal from tile {0}").format(current_tile) )
-    for tile_data in tiles_data:
-        try:
-            if 'result' in tile_data:
-                for data in tile_data['result']['map']:
-                    if 'error' in tile_data['result']['map'][data]:
-                        timed_out_items.append(data)
-                    else:
-                        for entry in tile_data['result']['map'][data]['gameEntities']:
-                            #print(entry)
-                            if entry[2][0] == 'p':
-                                portal_id.append(entry[0])
-                                portals.append(entry[2])
-                                #print(entry[0])
-        except:
-            print("could not parse all prtals")
-    return portals, portal_id
+#         iitc_tile_name  = ('{0}_{1}_{2}_0_8_100').format(zoom, iitc_xtile, iitc_ytile)
+#         current_tile = idx+1
+#         print(str("{0}/{1} Getting portals from tile : {2}").format(current_tile, total_tiles, iitc_tile_name))
+#         try:
+#             tiles_data.append(login.get_entities([iitc_tile_name]))
+#         except:
+#             print(str("Something went wrong while getting portal from tile {0}").format(current_tile) )
+#     for tile_data in tiles_data:
+#         try:
+#             if 'result' in tile_data:
+#                 for data in tile_data['result']['map']:
+#                     if 'error' in tile_data['result']['map'][data]:
+#                         timed_out_items.append(data)
+#                     else:
+#                         for entry in tile_data['result']['map'][data]['gameEntities']:
+#                             #print(entry)
+#                             if entry[2][0] == 'p':
+#                                 portal_id.append(entry[0])
+#                                 portals.append(entry[2])
+#                                 #print(entry[0])
+#         except:
+#             print("could not parse all prtals")
+#  return portals, portal_id
 
 if __name__ == "__main__":
     portal_name = 8
@@ -166,16 +166,16 @@ if __name__ == "__main__":
     print_configs(config)
 
     print("Initialize/Start DB Session")
-    mydb_r = connect(
-        host=config['db_r_host'],
-        user=config['db_r_user'],
-        passwd=config['db_r_pass'],
-        database=config['db_r_name'],
-        port=config['db_r_port'],
-        charset=config['db_r_charset'],
-        autocommit=True)
+   #  mydb_r = connect(
+   #      host=config['db_r_host'],
+   #      user=config['db_r_user'],
+   #      passwd=config['db_r_pass'],
+   #      database=config['db_r_name'],
+   #      port=config['db_r_port'],
+   #      charset=config['db_r_charset'],
+   #      autocommit=True)
 
-    mycursor_r = mydb_r.cursor()
+   # mycursor_r = mydb_r.cursor()
     print("Connection clear")
     updated_gyms = 0
     updated_pokestops = 0
@@ -188,214 +188,9 @@ if __name__ == "__main__":
             response = webhook.execute()
         sys.exit()
 
+
     if args.cellscore:
         cellscore = IngressLogin.get_region_score_details(110,112)
         print(cellscore)
 
-    if args.all_poi or args.ingress:
-        bbox = list(config['bbox'].split(';'))
-        tiles = []
-        for cord in bbox:
-            bbox_cord = list(map(float, cord.split(',')))
-            bbox_cord.append(zoom)
-            mTiles = MapTiles(bbox_cord)
-            tiles = tiles + mTiles.getTiles()
-        print(tiles)
-        total_tiles = len(tiles)
-        print("Number of tiles in boundry are : ",total_tiles)
-
-        all_portal_details, all_portals_id = get_all_portals(IngressLogin, tiles)
-        
-    if args.ingress:
-    
-        print("Initialize/Start DB Session")
-        mydb_r = connect(
-            host=config['db_r_host'],
-            user=config['db_r_user'],
-            passwd=config['db_r_pass'],
-            database=config['db_ingress'],
-            port=config['db_r_port'],
-            charset=config['db_r_charset'],
-            autocommit=True)
-
-        mycursor_ingres = mydb_r.cursor()
-        print("Connection clear")
-    
-        portal_update_query = PORTAL_UPDATE_QUERY.format(
-                db_ingress=config['db_ingress']
-            )
-        for idx, val in enumerate(all_portals_id):
-            lat = (all_portal_details[idx][2])/1e6
-            lon = (all_portal_details[idx][3])/1e6
-            p_name = (all_portal_details[idx][portal_name]).encode('utf-8')
-            p_url = all_portal_details[idx][portal_url]
-            updated_ts = datetime.datetime.now().strftime("%s")
-            insert_portal_args = (val,  p_name,  p_url, lat, lon, updated_ts, updated_ts, updated_ts, p_name,  p_url, lat, lon)
-            try:
-                mycursor_ingres.execute(portal_update_query, insert_portal_args)
-                print("~"*50)
-                print("inserted {0} into ingress table".format(p_name))
-                print("~"*50)
-            except Exception as e:
-                print(e)
-                print("#"*50)
-                print("could not put in db {0} {1} ".format(val, p_name))
-                print("#"*50)
-
-    if args.all_poi:
-        
-        gym_sel_query = GYM_SELECT_QUERY.format(
-                    db_gym_id=config['db_gym_id'],
-                    db_name=config['db_r_name'],
-                    db_gym=config['db_gym'],
-                    db_gym_name=config['db_gym_name']
-                )
-        mycursor_r.execute(gym_sel_query)
-        gym_result_ids = mycursor_r.fetchall()
-        
-        gym_update_query = GYM_UPDATE_QUERY.format(
-                db_name=config['db_r_name'],
-                db_gym=config['db_gym'],
-                db_gym_name=config['db_gym_name'],
-                db_gym_image=config['db_gym_image'],
-                db_gym_id=config['db_gym_id'],
-            )
-
-        for gym_id in gym_result_ids:
-            try:
-                single_portal_detail = all_portal_details[all_portals_id.index(gym_id[0])]
-                insert_args = (single_portal_detail[portal_name],  single_portal_detail[portal_url],  gym_id[0] )
-                try:
-                    mycursor_r.execute(gym_update_query, insert_args)
-                    updated_gyms = updated_gyms +1
-                    print(str("ID- {0} Name-{1}inserted {0} into ingress table".format(gym_id[0], single_portal_detail[portal_name])))
-                except Exception as e:
-                    print("~"*15)
-                    print(gym_id[0], ' Could not update in DB')
-                    print("~"*15)
-                    print(e)
-                    print("~"*15)
-            except Exception as e:
-                print(e)
-                print("Did not {0} find missing gym_id in given BBOX boundry".format(gym_id))
-        print('Total gyms updated: ', updated_gyms)
-
-    
-        pokestop_sel_query = POKESTOP_SELECT_QUERY.format(
-                    db_pokestop_id=config['db_pokestop_id'],
-                    db_name=config['db_r_name'],
-                    db_pokestop=config['db_pokestop'],
-                    db_pokestop_name=config['db_pokestop_name']
-                )
-        mycursor_r.execute(pokestop_sel_query)
-        pokestop_result_ids = mycursor_r.fetchall()
-
-        pokestop_update_query = POKESTOP_UPDATE_QUERY.format(
-                    db_name=config['db_r_name'],
-                    db_pokestop=config['db_pokestop'],
-                    db_pokestop_name=config['db_pokestop_name'],
-                    db_pokestop_image=config['db_pokestop_image'],
-                    db_pokestop_id=config['db_pokestop_id'],
-                )
-                
-        for stop_id in pokestop_result_ids:
-            try:
-                single_portal_detail = all_portal_details[all_portals_id.index(stop_id[0])]
-                insert_args = (single_portal_detail[portal_name],  single_portal_detail[portal_url],  stop_id[0] )                
-                try:
-                    mycursor_r.execute(pokestop_update_query, insert_args)
-                    updated_pokestops = updated_pokestops + 1
-                    print(stop_id[0], single_portal_detail[portal_name], ' succeeded and updated in DB')
-                except Exception as e:
-                    print("~"*15)
-                    print(stop_id[0], ' Could not update in DB')
-                    print("~"*15)
-                    print(e)
-                    print("~"*15)
-                    
-            except Exception as e:
-                print('Did not find missing pokestop_id in given BBOX boundry' )
-        print('Total pokestops updated: ', updated_pokestops)
-
-    else:
-        if args.gym:
-        
-            gym_sel_query = GYM_SELECT_QUERY.format(
-                        db_gym_id=config['db_gym_id'],
-                        db_name=config['db_r_name'],
-                        db_gym=config['db_gym'],
-                        db_gym_name=config['db_gym_name']
-                    )
-            mycursor_r.execute(gym_sel_query)
-            gym_result_ids = mycursor_r.fetchall()
-            
-            print('Total gyms found: ', len(gym_result_ids))
-            
-            gym_update_query = GYM_UPDATE_QUERY.format(
-                    db_name=config['db_r_name'],
-                    db_gym=config['db_gym'],
-                    db_gym_name=config['db_gym_name'],
-                    db_gym_image=config['db_gym_image'],
-                    db_gym_id=config['db_gym_id'],
-                )
-                
-            for gym_id in gym_result_ids:
-                ingress_portal_details = IngressLogin.get_portal_details(gym_id[0])
-                if ingress_portal_details is not None:
-                    insert_args = (ingress_portal_details.get('result')[portal_name],  ingress_portal_details.get('result')[portal_url],  gym_id[0] )
-                    try:
-                        mycursor_r.execute(gym_update_query, insert_args)
-                        updated_gyms = updated_gyms +1
-                        print(gym_id[0], ingress_portal_details.get('result')[portal_name], ingress_portal_details.get('result')[portal_url], ' succeeded and updated in DB')
-                    except Exception as e:
-                        print("~"*15)
-                        print(gym_id[0], ' Could not update in DB')
-                        print("~"*15)
-                        print(e)
-                        print("~"*15)
-                else:
-                    print('Could not parse portal info for ', gym_id[0], 'check if it is valid portal', )
-                    
-            print('Total gyms updated: ', updated_gyms)
-            
-        if args.pokestop:
-        
-            pokestop_sel_query = POKESTOP_SELECT_QUERY.format(
-                        db_pokestop_id=config['db_pokestop_id'],
-                        db_name=config['db_r_name'],
-                        db_pokestop=config['db_pokestop'],
-                        db_pokestop_name=config['db_pokestop_name']
-                    )
-            mycursor_r.execute(pokestop_sel_query)
-            pokestop_result_ids = mycursor_r.fetchall()
-            
-            print('Total pokestops found: ', len(pokestop_result_ids))
-            
-            pokestop_update_query = POKESTOP_UPDATE_QUERY.format(
-                        db_name=config['db_r_name'],
-                        db_pokestop=config['db_pokestop'],
-                        db_pokestop_name=config['db_pokestop_name'],
-                        db_pokestop_image=config['db_pokestop_image'],
-                        db_pokestop_id=config['db_pokestop_id'],
-                    )
-                    
-            for stop_id in pokestop_result_ids:
-                ingress_portal_details = IngressLogin.get_portal_details(stop_id[0])
-                if ingress_portal_details is not None:
-                    print(ingress_portal_details.get('result')[portal_name], ingress_portal_details.get('result')[portal_url])
-                    insert_args = (ingress_portal_details.get('result')[portal_name],  ingress_portal_details.get('result')[portal_url],  stop_id[0] )                
-                    try:
-                        mycursor_r.execute(pokestop_update_query, insert_args)
-                        print(stop_id[0], ingress_portal_details.get('result')[portal_name], ingress_portal_details.get('result')[portal_url], ' succeeded and updated in DB')
-                        updated_pokestops = updated_pokestops + 1
-                    except Exception as e:
-                        print("~"*15)
-                        print(stop_id[0], ' Could not update in DB')
-                        print("~"*15)
-                        print(e)
-                        print("~"*15)
-                        
-                else:
-                    print('Could not parse portal info for ', stop_id[0], 'check if it is valid portal', )
-                    
-            print('Total pokestops updated: ', updated_pokestops)
+ 
