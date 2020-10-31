2d6a78c2a09b9580b31b548b0dfad413be55688d
vreezy117 <vreezy117@googlemail.com>
2020-10-31 21:39:56 +0100

    added https

--- webserver.py
+++ webserver.py
@@ -29,7 +29,7 @@ def static_file(path):
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0")
+    app.run(debug=True,host="0.0.0.0",ssl_context=('cert.pem', 'key.pem'))
     #app.run(host="0.0.0.0", port=80)
 
 
